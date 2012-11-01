bcb567a9d4b88ab2ffbd97b4dbeeeec726108b8f
Daniel Wroblewski <daniel.wroblewski@gmail.com>
2012-11-01 18:17:39 +0100

    Added some basic css and spotify api calls

--- spitter/js/demo.js
+++ spitter/js/demo.js
@@ -1,48 +1,28 @@
-/*  
- *  Declare standard objects for the API (model/view)
- */
-var sp = getSpotifyApi(1);
-var m = sp.require('sp://import/scripts/api/models');
-var v = sp.require('sp://import/scripts/api/views');
-var dom = sp.require('sp://import/scripts/dom');
-
-var API_KEY = "change_me"; // Personal key obtained from Last.FM
-var API_REQ = "http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&format=json&api_key=" + API_KEY;
-
-var application = (function() {	
-	/* Initialize app on body.onload */
-	init = function() {
-		addLoader();
-		fetchLastFMData();
-	},
-	addLoader = function() {
-		var loaderHtml = '<div class="throbber"><div class="wheel"></div></div>';
-		$('.app').before(loaderHtml);
-	}
-})();
-
-
-function searchTwitter(query) {
-    $.ajax({
-        url: 'http://search.twitter.com/search.json?' + jQuery.param(query),
-        dataType: 'jsonp',
-        success: function(data) {
-            var tweets = $('#tweets');
-            tweets.html('');
-            for (res in data['results']) {
-                tweets.append('<div>' + data['results'][res]['from_user'] + ' wrote: <p>' + data['results'][res]['text'] + '</p></div><br />');
-        }
-        }
-    });
+function tabs() {
+    var args = models.application.arguments;
+    var current = document.getElementById(args[0]);
+    var sections = document.getElementsByClassName('section');
+    for (i=0;i<sections.length;i++){
+        sections[i].style.display = 'none';
+    }
+    current.style.display = 'block';
 }
 
-$(document).ready(function() {
-    $('#submit').click(function() {
-        var params = {
-            q: $('#query').val(),
-            rpp: 5
-        };
-        // alert(jQuery.param(params));
-        searchTwitter(params);
-    });
+var sp = getSpotifyApi(1);
+var models = sp.require('sp://import/scripts/api/models');
+var views = sp.require('sp://import/scripts/api/views');
+var dom = sp.require('sp://import/scripts/dom');
+var ui = sp.require("sp://import/scripts/ui");
+var application = models.application;
+var	playerImage = new views.Player();	
+
+$(function() {	
+	tabs();	
+	application.observe(models.EVENT.ARGUMENTSCHANGED, tabs);
+
+	var storage = window['localStorage'];
+    console.log(storage.getItem('name'));
+
+	localStorage.setItem('name', "Hello");
+    localStorage.setItem('uri', "World");
 });
